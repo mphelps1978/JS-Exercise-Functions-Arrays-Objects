0c82d88b9f72771e76821d073f4fb4b2f6ced326
mphelps1978 <phelps.michael@live.com>
2019-11-11 18:28:28 -0600

    17 Passed

--- index.js
+++ index.js
@@ -250,8 +250,14 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
  */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, maxYear) {
+  let newArray = [];
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year <= maxYear) {
+      newArray.push(inventory[i]);
+    }
+  }
+  return newArray;
 }
 
 /**
